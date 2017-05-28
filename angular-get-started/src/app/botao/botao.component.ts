3c0c4e3157d3ad67d0b6b39a207d244ac7f216a7
Jeferson Fadanelli <jeffefadanelli@gmail.com>
2017-05-27 22:42:51 -0300

    Começando routing

--- angular-get-started/src/app/botao/botao.component.ts
+++ angular-get-started/src/app/botao/botao.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-botao',
   template: `
-    <button type="submit" class="btn {{estiloBotao}}" (click)="clicou(funcao)"  >{{descricao}}</button>
+    <button type="submit" class="btn {{estiloBotao}}" (click)="clicou(funcao)" routerLink="/heroes" >{{descricao}}</button>
   `,
   styleUrls: ['./botao.component.scss']
 })
@@ -11,6 +12,7 @@ export class BotaoComponent implements OnInit {
   @Input() descricao: string;
   @Input() funcao: string;
   @Input() estiloBotao: string;
+  @Inject(Router) private router: Router;
 
   constructor() {
   }
@@ -23,6 +25,7 @@ export class BotaoComponent implements OnInit {
         //alert("Voltar");
     }else{
         //alert("Avançar");
+        this.router.navigate(['/hero']);
     }
   }
 }
