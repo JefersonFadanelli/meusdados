b3feada156c98176145c1cf1e8ad147aa6cf940f
Jeferson Fadanelli <jeffefadanelli@gmail.com>
2017-05-27 22:02:19 -0300

    Botões dinamicos

--- angular-get-started/src/app/botao/botao.component.ts
+++ angular-get-started/src/app/botao/botao.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-botao',
   template: `
-    <button type="submit" class="btn btn-success">Próximo</button>
+    <button type="submit" class="btn {{estiloBotao}}" (click)="clicou(funcao)"  >{{descricao}}</button>
   `,
   styleUrls: ['./botao.component.scss']
 })
 export class BotaoComponent implements OnInit {
+  @Input() descricao: string;
+  @Input() funcao: string;
+  @Input() estiloBotao: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  public clicou( funcaoT ) {
+    if(funcaoT === 'voltar'){
+        //alert("Voltar");
+    }else{
+        //alert("Avançar");
+    }
+  }
 }
--- angular-get-started/src/app/resposta/resposta.component.ts
+++ angular-get-started/src/app/resposta/resposta.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-resposta',
   template: `
-    <input type="text" class="form-control" id="input-respsota">
+    <input type="text" class=" col-md-6 col-sm-offset-3 col-sm-10 col-sm-offset-1 col-xs-10 col-xs-offset-1" id="input-respsota">
   `,
   styleUrls: ['./resposta.component.scss']
 })
